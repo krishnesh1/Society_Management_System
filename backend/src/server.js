e23decf4f89b746c23c311b7b3b7d2db3cab1789
Krishnesh Karn <karnkrishnesh49@gmail.com>
2026-07-06 20:57:04 +0530

    Update server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -21,7 +21,7 @@ import residentRoutes from "./routes/residentRoutes.js";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8000;
 
 app.use(helmet());
 app.use(
